f4525babecc188cf54a479cec3007a61ce2311f9
Anton Golosnichenko <golosnichenko@artics.ru>
2021-10-11 07:23:01 +0300

    14.1 Переход между экранами

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-export default function LogIn() {
+export default function LogIn({ navigation }) {
   return (
     <View>
       <Text>Вход в аккаунт</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("CreateAccount");
+        }}
+      >
+        <View>
+          <Text>Перейти к созданию аккаунта</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,29 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-export default function Welcome() {
+export default function Welcome({ navigation }) {
   return (
     <View>
       <Text>Добро пожаловать!</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("CreateAccount");
+        }}
+      >
+        <View>
+          <Text>Создать аккаунт</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("LogIn");
+        }}
+      >
+        <View>
+          <Text>Войти в аккаунт</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
