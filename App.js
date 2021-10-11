c49a9a500f1e51832a206141df7b7bc92198c597
Anton Golosnichenko <golosnichenko@artics.ru>
2021-10-11 04:56:35 +0300

    13.7 AppLoading - часть 2

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -11,11 +12,16 @@ export default function App() {
   const onLoading = () => {
     const fontsToLoad = [Ionicons.font];
     const fontsPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-    return Promise.all(fontsPromises);
+    const imagesToLoad = [
+      require("./assets/logo.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+    ];
+    const imagesPromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontsPromises, ...imagesPromises]);
   };
 
   const onFinish = () => {
-    console.log("Шрифты загружены");
+    console.log("Шрифты и картинки загружены");
     setLoading(false);
   };
 
