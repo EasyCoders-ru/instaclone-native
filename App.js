e4eca30eeea9c2830e48f842d880bf34c37612e4
Anton Golosnichenko <golosnichenko@artics.ru>
2021-10-11 04:06:50 +0300

    13.6 AppLoading

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  const onLoading = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontsPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    return Promise.all(fontsPromises);
+  };
+
+  const onFinish = () => {
+    console.log("Шрифты загружены");
+    setLoading(false);
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={onLoading}
+        onFinish={onFinish}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Привет, кодеры!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
