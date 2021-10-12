1787cdfe4b9bfd67644196e9a66e628358ddf3cc
Anton Golosnichenko <golosnichenko@artics.ru>
2021-10-13 02:41:57 +0300

    14.3 Темная тема

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, useColorScheme } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Welcome({ navigation }) {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
+
   return (
-    <View>
-      <Text>Добро пожаловать!</Text>
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text style={[styles.text, themeTextStyle]}>Добро пожаловать!</Text>
       <TouchableOpacity
         onPress={() => {
           navigation.navigate("CreateAccount");
         }}
       >
-        <View>
-          <Text>Создать аккаунт</Text>
+        <View style={[styles.container, themeContainerStyle]}>
+          <Text style={[styles.text, themeTextStyle]}>Создать аккаунт</Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity
@@ -20,10 +27,30 @@ export default function Welcome({ navigation }) {
           navigation.navigate("LogIn");
         }}
       >
-        <View>
-          <Text>Войти в аккаунт</Text>
+        <View style={[styles.container, themeContainerStyle]}>
+          <Text style={[styles.text, themeTextStyle]}>Войти в аккаунт</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  lightContainer: {
+    backgroundColor: "#d0d0c0",
+  },
+  darkContainer: {
+    backgroundColor: "#242c40",
+  },
+  lightThemeText: {
+    color: "#242c40",
+  },
+  darkThemeText: {
+    color: "#d0d0c0",
+  },
+});
