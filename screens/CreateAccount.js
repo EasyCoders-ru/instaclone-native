36120bce8fb2317d249a6ccea7b55a00c1d3e3da
Anton Golosnichenko <golosnichenko@artics.ru>
2021-10-16 06:41:05 +0300

    14.7 Работа с формой - часть 1

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import { TextInput } from "react-native";
 import { AuthButton } from "../components/auth/AuthButton";
 import { AuthLayout } from "../components/auth/AuthLayout";
 
 export default function CreateAccount() {
+  const lastNameRef = useRef();
+  const loginRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const onNext = (nextRef) => {
+    nextRef?.current?.focus();
+  };
+
+  const onDone = () => {
+    alert("Форма отправлена!");
+  };
+
   return (
     <AuthLayout>
       <TextInput
@@ -11,27 +24,43 @@ export default function CreateAccount() {
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => {
+          onNext(lastNameRef);
+        }}
       />
       <TextInput
+        ref={lastNameRef}
         placeholder="Фамилия"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => {
+          onNext(loginRef);
+        }}
       />
       <TextInput
+        ref={loginRef}
         placeholder="Логин"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => {
+          onNext(emailRef);
+        }}
       />
       <TextInput
+        ref={emailRef}
         placeholder="Email"
         placeholderTextColor="gray"
         returnKeyType="next"
         keyboardType="email-address"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => {
+          onNext(passwordRef);
+        }}
       />
       <TextInput
+        ref={passwordRef}
         placeholder="Пароль"
         placeholderTextColor="gray"
         returnKeyType="next"
