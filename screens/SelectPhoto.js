5f10cb708167d337ed0095cbce7004d9ff068d75
Anton Golosnichenko <golosnichenko@artics.ru>
2021-10-24 02:24:01 +0300

    17.3 Выбор фото - часть 2

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
+import * as MediaLibrary from "expo-media-library";
 
 const Container = styled.View``;
 const Top = styled.View``;
 const Bottom = styled.View``;
 
 export default function SelectPhoto() {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const getPhotos = async () => {
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync();
+      setPhotos(photos);
+    }
+  };
+
+  const getPermissions = async () => {
+    const { accessPrivileges, canAskAgain } =
+      await MediaLibrary.getPermissionsAsync();
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.getPermissionsAsync();
+      if (accessPrivileges !== "none") {
+        setOk(true);
+      }
+    } else if (accessPrivileges !== "none") {
+      setOk(true);
+    }
+  };
+
+  useEffect(() => {
+    await getPermissions();
+    await getPhotos();
+  }, [getPermissions, getPhotos]);
+
   return (
     <Container>
       <Top></Top>
