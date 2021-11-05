0a19e25369cc5688396b4ae8b2ee681743ae04c2
Anton Golosnichenko <golosnichenko@artics.ru>
2021-11-05 20:56:44 +0300

    18.2 Список диалогов - часть 2

--- screens/Dialogs.js
+++ screens/Dialogs.js
@@ -5,6 +5,7 @@ import { gql, useQuery } from "@apollo/client";
 import { DIALOG_FRAGMENT } from "../fragments";
 import ScreenLayout from "../components/ScreenLayout";
 import styled from "styled-components/native";
+import { colors } from "../colors";
 
 const SEE_DIALOGS_QUERY = gql`
   query seeDialogs {
@@ -16,31 +17,82 @@ const SEE_DIALOGS_QUERY = gql`
 `;
 
 const DialogContainer = styled.View`
+  width: 100%;
+  padding: 15px 10px;
   background-color: black;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const DialogText = styled.Text`
+const UnreadText = styled.Text`
+  color: white;
+  margin-top: 10px;
+  font-weight: 500;
+`;
+
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 20px;
+`;
+
+const Username = styled.Text`
+  font-weight: 600;
+  font-size: 16px;
   color: white;
 `;
 
+const Data = styled.View``;
+
+const Column = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+`;
+
+const UnreadDot = styled.View`
+  height: 10px;
+  width: 10px;
+  border-radius: 5px;
+  background-color: ${colors.blue};
+`;
+
 export default function Dialogs({ navigation }) {
-  const { data: userData } = useMe();
+  const { data: meData } = useMe();
 
   const { data, loading } = useQuery(SEE_DIALOGS_QUERY);
 
   useEffect(() => {
-    navigation.setOptions({ title: userData?.me?.username });
-  }, [userData?.me?.username]);
+    navigation.setOptions({ title: meData?.me?.username });
+  }, [meData?.me?.username]);
 
-  const renderItem = ({ item: dialog }) => (
-    <DialogContainer>
-      <DialogText>
-        {dialog.unreadTotal === 0
-          ? "Имя пользователя"
-          : `${dialog.unreadTotal} непрочитанных сообщения`}
-      </DialogText>
-    </DialogContainer>
-  );
+  const renderItem = ({ item: dialog }) => {
+    const notMe = dialog?.users.find(
+      (user) => user.username !== meData?.me?.username
+    );
+
+    return (
+      <DialogContainer>
+        <Column>
+          <Avatar source={{ uri: notMe.avatar }} />
+          <Data>
+            <Username>{notMe.username}</Username>
+            {dialog.unreadTotal > 0 && (
+              <UnreadText>
+                {dialog.unreadTotal === 1
+                  ? "1 непрочитанное сообщение"
+                  : `${dialog.unreadTotal} непрочитанных сообщений`}
+              </UnreadText>
+            )}
+          </Data>
+        </Column>
+        <Column>{dialog.unreadTotal > 0 && <UnreadDot />}</Column>
+      </DialogContainer>
+    );
+  };
 
   return (
     <ScreenLayout loading={loading}>
