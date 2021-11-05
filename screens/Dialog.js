0db5339431ff4e032eee290fad997af5c4be0161
Anton Golosnichenko <golosnichenko@artics.ru>
2021-11-05 23:39:39 +0300

    18.4 Окно диалога - часть 2

--- screens/Dialog.js
+++ screens/Dialog.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { FlatList, KeyboardAvoidingView, TextInput } from "react-native";
 import { gql, useQuery } from "@apollo/client";
 import { DIALOG_FRAGMENT } from "../fragments";
+import ScreenLayout from "../components/ScreenLayout";
+import styled from "styled-components/native";
 
 const SEE_DIALOG_QUERY = gql`
   query seeDialog($id: Int!) {
@@ -22,6 +24,16 @@ const SEE_DIALOG_QUERY = gql`
   ${DIALOG_FRAGMENT}
 `;
 
+const MessageContainer = styled.View``;
+const Author = styled.View``;
+const Avatar = styled.Image``;
+const Username = styled.Text`
+  color: white;
+`;
+const Message = styled.Text`
+  color: white;
+`;
+
 export default function Dialog({ route, navigation }) {
   const { data, loading } = useQuery(SEE_DIALOG_QUERY, {
     variables: { id: route?.params?.id },
@@ -33,9 +45,36 @@ export default function Dialog({ route, navigation }) {
     });
   });
 
+  const renderItem = ({ item: message }) => (
+    <MessageContainer>
+      <Author>
+        <Avatar source={{ uri: message?.user?.avatar }} />
+        <Username>{message?.user?.username}</Username>
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
+
   return (
-    <View>
-      <Text>Окно диалога</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ backgroundColor: "black", flex: 1 }}
+      behavior="height"
+      keyboardVerticalOffset={100}
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          data={data?.seeDialog?.messages}
+          keyExtractor={(message) => "" + message.id}
+          renderItem={renderItem}
+          style={{ width: "100%" }}
+          inverted
+        />
+        <TextInput
+          placeholder="Введите текст сообщения"
+          returnKeyType="send"
+          returnKeyLabel="Отправить"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 }
