fd8e990cdd7e9e6cf6e5aaf815c3cc4708fdc41e
Anton Golosnichenko <golosnichenko@artics.ru>
2021-11-05 23:14:12 +0300

    18.3 Окно диалога - часть 1

--- screens/Dialog.js
+++ screens/Dialog.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { DIALOG_FRAGMENT } from "../fragments";
 
-export default function Dialog() {
+const SEE_DIALOG_QUERY = gql`
+  query seeDialog($id: Int!) {
+    seeDialog(id: $id) {
+      ...DialogParts
+      messages {
+        id
+        payload
+        user {
+          id
+          username
+          avatar
+        }
+        read
+      }
+    }
+  }
+  ${DIALOG_FRAGMENT}
+`;
+
+export default function Dialog({ route, navigation }) {
+  const { data, loading } = useQuery(SEE_DIALOG_QUERY, {
+    variables: { id: route?.params?.id },
+  });
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: route?.params?.talkingTo?.username,
+    });
+  });
+
   return (
     <View>
       <Text>Окно диалога</Text>
--- screens/Dialogs.js
+++ screens/Dialogs.js
@@ -4,8 +4,8 @@ import useMe from "../hooks/useMe";
 import { gql, useQuery } from "@apollo/client";
 import { DIALOG_FRAGMENT } from "../fragments";
 import ScreenLayout from "../components/ScreenLayout";
-import styled from "styled-components/native";
-import { colors } from "../colors";
+
+import DialogItem from "../components/dialogs/DialogItem";
 
 const SEE_DIALOGS_QUERY = gql`
   query seeDialogs {
@@ -14,50 +14,6 @@ const SEE_DIALOGS_QUERY = gql`
     }
   }
   ${DIALOG_FRAGMENT}
-`;
-
-const DialogContainer = styled.View`
-  width: 100%;
-  padding: 15px 10px;
-  background-color: black;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const UnreadText = styled.Text`
-  color: white;
-  margin-top: 10px;
-  font-weight: 500;
-`;
-
-const Avatar = styled.Image`
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
-  margin-right: 20px;
-`;
-
-const Username = styled.Text`
-  font-weight: 600;
-  font-size: 16px;
-  color: white;
-`;
-
-const Data = styled.View``;
-
-const Column = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 10px;
-`;
-
-const UnreadDot = styled.View`
-  height: 10px;
-  width: 10px;
-  border-radius: 5px;
-  background-color: ${colors.blue};
 `;
 
 export default function Dialogs({ navigation }) {
@@ -69,30 +25,7 @@ export default function Dialogs({ navigation }) {
     navigation.setOptions({ title: meData?.me?.username });
   }, [meData?.me?.username]);
 
-  const renderItem = ({ item: dialog }) => {
-    const notMe = dialog?.users.find(
-      (user) => user.username !== meData?.me?.username
-    );
-
-    return (
-      <DialogContainer>
-        <Column>
-          <Avatar source={{ uri: notMe.avatar }} />
-          <Data>
-            <Username>{notMe.username}</Username>
-            {dialog.unreadTotal > 0 && (
-              <UnreadText>
-                {dialog.unreadTotal === 1
-                  ? "1 непрочитанное сообщение"
-                  : `${dialog.unreadTotal} непрочитанных сообщений`}
-              </UnreadText>
-            )}
-          </Data>
-        </Column>
-        <Column>{dialog.unreadTotal > 0 && <UnreadDot />}</Column>
-      </DialogContainer>
-    );
-  };
+  const renderItem = ({ item: dialog }) => <DialogItem {...dialog} />;
 
   return (
     <ScreenLayout loading={loading}>
